266751eaf5a5f9b6df570a1f503ec08dc0c3ac87
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-07-22 19:46:40 -0400

    fix bugs

--- expression_templates.h
+++ expression_templates.h
@@ -6,19 +6,21 @@ namespace expression_templates
 {
 	namespace impl
 	{
+		template<typename funct_ty, typename...args_ty>
+		auto call(funct_ty&& funct, args_ty&&...args);
+
 		template<typename t>
 		class value
 		{
 			template<typename t>
 			friend value<t> take(t&& a);
 			template<typename funct_ty, typename...args_ty>
-			friend auto call(funct_ty&& funct, args_ty&&...args);
+			friend auto expression_templates::impl::call(funct_ty&& funct, args_ty&&...args);
 		public:
 
-			value(value&& a)
-			{
-				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
-			}
+			value(value&& a) :
+				held(std::move(a.held))
+			{}
 
 			t operator()() const
 			{
@@ -33,10 +35,9 @@ namespace expression_templates
 				held(std::move(a))
 			{}
 
-			value(value const& a)
-			{
-				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
-			}
+			value(value const& a) :
+				held(std::move(a.held)) //in order to lambda capture variadic lists by move without tuples
+			{}
 
 			void operator=(value&&) = delete;
 			void operator=(value const&) = delete;
@@ -50,7 +51,7 @@ namespace expression_templates
 			template<typename t>
 			friend value<t&> ref(t& a);
 			template<typename funct_ty, typename...args_ty>
-			friend auto call(funct_ty&& funct, args_ty&&...args);
+			friend auto expression_templates::impl::call(funct_ty&& funct, args_ty&&...args);
 		public:
 
 			value(value&& a)
@@ -88,7 +89,7 @@ namespace expression_templates
 			template<typename t>
 			friend value<t&&> use(t&& a);
 			template<typename funct_ty, typename...args_ty>
-			friend auto call(funct_ty&& funct, args_ty&&...args);
+			friend auto expression_templates::impl::call(funct_ty&& funct, args_ty&&...args);
 		public:
 
 			value(value&& a)
@@ -124,13 +125,12 @@ namespace expression_templates
 		class result
 		{
 			template<typename funct_ty, typename...args_ty>
-			friend auto call(funct_ty&& funct, args_ty&&...args);
+			friend auto expression_templates::impl::call(funct_ty&& funct, args_ty&&...args);
 		public:
 
-			result(result&& a)
-			{
-				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
-			}
+			result(result&& a) :
+				held(std::move(a.held))
+			{}
 
 			decltype(std::declval<lambda_ty>()())
 				operator()() const
@@ -146,10 +146,9 @@ namespace expression_templates
 				held(std::move(a))
 			{}
 
-			result(result const& a)
-			{
-				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
-			}
+			result(result const& a) :
+				held(std::move(a.held)) //in order to lambda capture variadic lists by move without tuples
+			{}
 
 			void operator=(result&&) = delete;
 			void operator=(result const&) = delete;
@@ -211,7 +210,7 @@ namespace expression_templates
 		}
 
 		template<typename funct_ty, typename...args_ty>
-		auto call(funct_ty&& funct, args_ty&&...args)
+		inline auto call(funct_ty&& funct, args_ty&&...args)
 		{
 			static_assert(and_all<types<args_ty>::is_template()...>(),"arguments of \"call\" must be created through \"take\", \"ref\", \"use\", or another \"call\"");
 			
