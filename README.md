5734a6fe549d4fb2d31a7d92ee8bab8245dd2d1e
Evan McGorty <evanjamesmcgorty@gmail.com>
2018-07-22 18:38:20 -0400

    rework everything (seems to be working), update readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # easy-expression-templates
 a set of functions that make it incredibly easy to use expression templates
 
-there is not much to say about this project. currently, the only two functions are "call" and "take".
-call takes a callable object and arguments to call it with. the result should be stored with auto, as it represents a theoretical concept for an object containing a result.
-"take" will cause the expression template to be evaluated, and is the transition from expression-templaty code into normal, stateful code. use it when a value must be stored.
-take will also work on a plain value, so it can be used with generic code when an argument can be either a raw value or an expression template.
+There is not much to say about this project. It is an attempt to allow the creation of generic expression templates.
+Currently, the only functions are "call", "take", "ref", and "use".
+They all produce an object that, when invoked without any arguments, will return a value. Their return types all match an imaginary "expression template" concept that should be implemented when concepts are out. 
+Their results should, for now, be taken with auto.
+
+"call" will take an invokable object, followed by arguments wrapped by "take", "ref", "use", and "call" itself.
+"take" is like passing by value. It will take (via move construction) and store the passed object until the template is evaluated, at which point it is moved into the function call.
+"ref" is like passing by reference. It keeps a pointer to the passed object and dereferences it when the expression template is evaluated.
+"use" is like passing by r-value reference, with a very similar use case to take. It keeps a pointer to the passed object, dereferences it, and then std::moves it when the template is evaluated.
--- expression_templates.h
+++ expression_templates.h
@@ -7,157 +7,225 @@ namespace expression_templates
 	namespace impl
 	{
 		template<typename t>
-		using return_type_t = std::result_of_t<t>;
-
-		template<typename funct_t>
-		class delayed_result
+		class value
 		{
 			template<typename t>
-			friend struct taker;
+			friend value<t> take(t&& a);
+			template<typename funct_ty, typename...args_ty>
+			friend auto call(funct_ty&& funct, args_ty&&...args);
 		public:
-			delayed_result(funct_t&& a) :
-				res(std::move(a))
-			{}
 
-			delayed_result(delayed_result&&) = default;
-			delayed_result(delayed_result const&) = default;
+			value(value&& a)
+			{
+				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
+			}
+
+			t operator()() const
+			{
+				return std::move(held);
+			}
+			
+			~value() = default;
 
 		private:
-			funct_t res;
+
+			value(t&& a) :
+				held(std::move(a))
+			{}
+
+			value(value const& a)
+			{
+				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
+			}
+
+			void operator=(value&&) = delete;
+			void operator=(value const&) = delete;
+
+			mutable t held;
 		};
 
 		template<typename t>
-		struct taken
+		class value<t&>
 		{
-			using type = t&;
+			template<typename t>
+			friend value<t&> ref(t& a);
+			template<typename funct_ty, typename...args_ty>
+			friend auto call(funct_ty&& funct, args_ty&&...args);
+		public:
+
+			value(value&& a)
+			{
+				held = a.held;
+			}
+
+			t& operator()() const
+			{
+				return *held;
+			}
+
+			~value() = default;
+		
+		private:
+
+			value(t& a) :
+				held(&a)
+			{}
+
+			value(value const& a)
+			{
+				held = a.held;
+			}
+
+			void operator=(value&&) = delete;
+			void operator=(value const&) = delete;
+
+			mutable t* held;
 		};
 
 		template<typename t>
-		struct taken<t&>
+		class value<t&&>
 		{
-			using type = t&;
+			template<typename t>
+			friend value<t&&> use(t&& a);
+			template<typename funct_ty, typename...args_ty>
+			friend auto call(funct_ty&& funct, args_ty&&...args);
+		public:
+
+			value(value&& a)
+			{
+				held = a.held;
+			}
+
+			t&& operator()() const
+			{
+				return std::move(*held);
+			}
+			
+			~value() = default;
+
+		private:
+
+			value(t&& a) :
+				held(&a)
+			{}
+
+			value(value const& a)
+			{
+				held = a.held;
+			}
+
+			void operator=(value&&) = delete;
+			void operator=(value const&) = delete;
+
+			mutable t* held;
+		};
+
+		template<typename lambda_ty>
+		class result
+		{
+			template<typename funct_ty, typename...args_ty>
+			friend auto call(funct_ty&& funct, args_ty&&...args);
+		public:
+
+			result(result&& a)
+			{
+				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
+			}
+
+			decltype(std::declval<lambda_ty>()())
+				operator()() const
+			{
+				return std::move(held)();
+			}
+			
+			~result() = default;
+
+		private:
+
+			result(lambda_ty&& a) :
+				held(std::move(a))
+			{}
+
+			result(result const& a)
+			{
+				held = std::move(a.held); //in order to lambda capture variadic lists by move without tuples
+			}
+
+			void operator=(result&&) = delete;
+			void operator=(result const&) = delete;
+
+			mutable lambda_ty held;
+		};
+
+
+		template<typename t>
+		struct types
+		{
+			static constexpr bool is_template()
+			{
+				return false;
+			}
 		};
 
 		template<typename t>
-		struct taken<delayed_result<t>>
+		struct types<value<t>>
 		{
-			using type = return_type_t<t>;
+			static constexpr bool is_template()
+			{
+				return true;
+			}
 		};
 
 		template<typename t>
-		struct taken<delayed_result<t>&>
+		struct types<result<t>>
 		{
-			using type = return_type_t<t>;
+			static constexpr bool is_template()
+			{
+				return true;
+			}
 		};
 
 		template<typename t>
-		struct taker
+		value<t> take(t&& a)
 		{
-			static t& on(t& a)
-			{
-				return a;
-			}
-
-			static t& unprepare(t& a)
-			{
-				return a;
-			}
-
-			static t prepare(t&& a)
-			{
-				return a;
-			}
-
-			using prepared_t = t&&;
-		};
+			return value<t>(std::move(a));
+		}
 
 		template<typename t>
-		struct taker<t&>
+		value<t&> ref(t& a)
 		{
-			static t& on(t& a)
-			{
-				return a;
-			}
-
-			static t& unprepare(t* a)
-			{
-				return *a;
-			}
-
-			static t* prepare(t& a)
-			{
-				return &a;
-			}
-			using prepared_t = t*&&;
-		};
+			return value<t&>(a);
+		}
 
 		template<typename t>
-		struct taker<delayed_result<t>>
+		value<t&&> use(t&& a)
 		{
-			static auto on(delayed_result<t>& a)
-			{
-				return std::move(a.res)();
-			}
-
-			static delayed_result<t>& unprepare(delayed_result<t>& a)
-			{
-				return a;
-			}
-
-			static delayed_result<t> prepare(delayed_result<t>&& a)
-			{
-				return a;
-			}
-
-			using prepared_t = delayed_result<t>&&;
-		};
-
-		template<typename t>
-		struct taker<delayed_result<t>&>
-		{
-			static auto on(delayed_result<t>& a)
-			{
-				return std::move(a.res)();
-			}
-
-			static delayed_result<t>& unprepare(delayed_result<t>* a)
-			{
-				return *a;
-			}
-
-			static delayed_result<t>* prepare(delayed_result<t>& a)
-			{
-				return &a;
-			}
-
-			using prepared_t = delayed_result<t>*&&;
-		};
-
-		template<typename t>
-		typename taken<t>::type take(t& a)
-		{
-			return taker<t>::on(a);
+			return value<t&&>(std::move(a));
 		}
 
 
-		template<typename funct_t, typename...args_t>
-		auto call(funct_t&& to_call, args_t&&...args)
+		template<bool a = true, bool...r>
+		constexpr bool and_all()
 		{
-			auto&& lambda =
-			std::bind
-			(
-				[](funct_t&& l_to_call, typename taker<args_t>::prepared_t...l_args) mutable
-				{
-					return std::move(l_to_call)(std::forward<typename taken<args_t>::type>(take<args_t>(taker<args_t>::unprepare(l_args)))...);
-				},
-				std::move(to_call),
-				taker<args_t>::prepare(std::forward<args_t>(args))...
-			);
+			return (sizeof...(r)==0)  ?  (a)  :  (a && and_all<r...>());
+		}
 
-			return delayed_result<std::remove_reference_t<decltype(lambda)>>(std::move(lambda));
+		template<typename funct_ty, typename...args_ty>
+		auto call(funct_ty&& funct, args_ty&&...args)
+		{
+			static_assert(and_all<types<args_ty>::is_template()...>(),"arguments of \"call\" must be created through \"take\", \"ref\", \"use\", or another \"call\"");
+			
+			auto&& lambda =
+			[=,funct = std::move(funct)]() mutable
+			{
+				return funct(args()...);
+			};
+			return result<std::remove_reference_t<decltype(lambda)>>(std::move(lambda));
 		}
 	}
 
-	using impl::call;
 	using impl::take;
+	using impl::ref;
+	using impl::use;
+	using impl::call;
 }
